f86cd74bfbbf71bf16880d618ec4663e1c2aa69b
emilyhmarkowitz <emilymarkowitz@gmail.com>
2020-10-21 14:54:22 -0400

    Addition of RMarkReports Package + other fun edits to text

--- rscripts/functions.R
+++ rscripts/functions.R
@@ -5,28 +5,9 @@
 #' start date: 2020-10
 #' ---
 
-CapStr <- function(y) {
-  c <- strsplit(y, " ")[[1]]
-  paste(toupper(substring(c, 1,1)), substring(c, 2),
-        sep="", collapse=" ")
-}
-
-
-funct_list<-function(x) {
-  x<-x[which(x!="")]
-  # x<-x[which(!is.null(x))]
-  x<-x[which(!is.na(x))]
-  # x<-x[order(x)]
-  if (length(x)==2) { 
-    str1<-paste(x, collapse = " and ")
-  } else if (length(x)>2) {
-    str1<-paste(x[1:(length(x)-1)], collapse = ", ")
-    str1<-paste0(str1, ", and ", x[length(x)])
-  } else {
-    str1<-x
-  }
-  return(str1)
-}
+# library(devtools)
+# devtools::install_github("emilyhmarkowitz/RMarkReports")
+library(RMarkReports)
 
 
 # General stuff
