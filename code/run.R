b7d5370f0f42609e155a60e0a600dc26e94d29e7
emilyhmarkowitz <emilymarkowitz@gmail.com>
2021-12-03 12:54:45 -0500

    minor update to annual reerence change

--- code/run.R
+++ code/run.R
@@ -24,7 +24,7 @@ usePNGPDF <- "png"
 # SRVY<-"EBS"
 # ref_compareyr_ebs <- "@RN976" # temp
 # ref_compareyr_nbs <- "@RN909" # temp
-# ref_maxyr_npfmc <- "@RN909" # temp
+# ref_maxyr_npfmc <- "@NPFMC2015" # temp
 
 # maxyr <- 2017 # or the year of the report, for example
 # compareyr <- 2010
@@ -32,7 +32,7 @@ usePNGPDF <- "png"
 # SRVY<-"NEBS"
 ref_compareyr_ebs <- "@RN976"
 ref_compareyr_nbs <- "@RN909"
-ref_maxyr_npfmc <- "@RN909" # temp
+# ref_maxyr_npfmc <- "@NPFMC2016" # temp
 dir_googledrive <- "1vtwfDwRprFml_5wN_WkeVViynwGhC8fe" # https://drive.google.com/drive/folders/1vtwfDwRprFml_5wN_WkeVViynwGhC8fe?usp=sharing
 
 # maxyr <- 2018 # NOTE RAPID RESPONCE
@@ -40,7 +40,7 @@ dir_googledrive <- "1vtwfDwRprFml_5wN_WkeVViynwGhC8fe" # https://drive.google.co
 # SRVY<-"NEBS"
 # ref_compareyr_ebs <- "@RN976" # CHANGE
 # ref_compareyr_nbs <- NA # CHANGE
-# ref_maxyr_npfmc <- "@RN909" # temp
+# ref_maxyr_npfmc <- "@NPFMC2017" # temp
 # dir_googledrive <- "1W8VfqBF9j48vk0GpFLyg5cZGzuHlelAy" # https://drive.google.com/drive/folders/1W8VfqBF9j48vk0GpFLyg5cZGzuHlelAy?usp=sharing
 
 # maxyr <- 2019 
@@ -48,12 +48,15 @@ dir_googledrive <- "1vtwfDwRprFml_5wN_WkeVViynwGhC8fe" # https://drive.google.co
 # SRVY<-"NEBS" 
 # ref_compareyr_ebs <- "@RN976" # CHANGE
 # ref_compareyr_nbs <- NA # CHANGE
-# ref_maxyr_npfmc <- "@RN909" # temp
+# ref_maxyr_npfmc <- "@NPFMC2018" # temp
 # dir_googledrive <- "1HpuuIIp6piS3CieRJR81-8hVJ3QaKOU" # https://drive.google.com/drive/folders/1HpuuIIp6piS3CieRJR81-8hVJ3QaKOU-?usp=sharing
 
 maxyr <- 2021
 compareyr <- 2019
 SRVY<-"NEBS"
+ref_compareyr_ebs <- "@RN976" # CHANGE
+ref_compareyr_nbs <- "@RN976" # CHANGE
+ref_maxyr_npfmc <- "@NPFMC2019" # temp
 dir_googledrive <- "1i3NRmaAPpIYfMI35fpJCa-8AjefJ7J7X" # https://drive.google.com/drive/folders/1i3NRmaAPpIYfMI35fpJCa-8AjefJ7J7X?usp=sharing
 
 # *** SIGN INTO GOOGLE DRIVE----------------------------------------------------
