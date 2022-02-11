d09d99c7991ce9355f846ee2dcb651da937996d4
emilymarkowitz-noaa <emily.markowitz@noaa.gov>
2022-02-11 11:33:32 -0800

    updated data_dl.R with RACEBASE.HAUL.start_time to character SQL ifelse

--- code/data_dl.R
+++ code/data_dl.R
@@ -11,6 +11,22 @@
 # source("C:/Users/emily.markowitz/Work/Projects/ConnectToOracle.R")
 source("C:/Users/emily.markowitz/Documents/Projects/ConnectToOracle.R")
 
+# I set up a ConnectToOracle.R that looks like this: 
+#   
+#   PKG <- c("RODBC")
+# for (p in PKG) {
+#   if(!require(p,character.only = TRUE)) {  
+#     install.packages(p)
+#     require(p,character.only = TRUE)}
+# }
+# 
+# channel<-odbcConnect(dsn = "AFSC",
+#                      uid = "USERNAME", # change
+#                      pwd = "PASSWORD", #change
+#                      believeNRows = FALSE)
+# 
+# odbcGetInfo(channel)
+
 ##################DOWNLOAD CPUE and BIOMASS EST##################################
 
 locations<-c(
@@ -58,7 +74,17 @@ print(Sys.Date())
 
 for (i in 1:length(locations)){
   print(locations[i])
-  a<-RODBC::sqlQuery(channel, paste0("SELECT * FROM ", locations[i]))
+  if (locations[i] == "RACEBASE.HAUL") { # that way I can also extract TIME
+
+    a<-RODBC::sqlQuery(channel, paste0("SELECT * FROM ", locations[i]))
+    
+    a<-RODBC::sqlQuery(channel, paste0("SELECT ",
+                                       paste0(names(a)[names(a) != "START_TIME"], sep = ",", collapse = " "),
+                                       " TO_CHAR(START_TIME,'MM/DD/YYYY HH24:MI:SS') START_TIME  FROM ", 
+                                       locations[i]))
+  } else {
+    a<-RODBC::sqlQuery(channel, paste0("SELECT * FROM ", locations[i]))
+  }
   write.csv(x=a, 
             paste0("./data/",
                    tolower(strsplit(x = locations[i], 
