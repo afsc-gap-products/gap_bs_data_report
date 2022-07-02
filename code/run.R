5e6d6f61cda68b14ffc72429439122e9d673c0cb
emilymarkowitz-noaa <emily.markowitz@noaa.gov>
2022-07-01 21:41:10 -0700

    aesthetic table and figure edits

--- code/run.R
+++ code/run.R
@@ -9,15 +9,17 @@
 
 # START ------------------------------------------------------------------------
 
-# issues
+# CHECK EVERY YEAR ---------------
+# each year we need these new bibs
+# paste0("@SAPcrab", maxyr)
+# paste0("@NPFMC", maxyr)
+
+# ISSUES -------------------------
 # had to recalculate the lencount column for crab data in species specific biomass/pop tables
 # make sure if spp found in NBS are only are plotted in the NBS, visa versa
 # create PA plots for low count spp
-
 # mgcv spline coefficient
 # no catch in idws
-
-# make sure that there is a citation for paste0("@NPFMC", maxyr)
 
 # *** REPORT KNOWNS ------------------------------------------------------------
 report_title <- "Data Report" # Fake until I get a better idea of how to automate something down the line
@@ -38,12 +40,12 @@ font0 <- "Arial Narrow"
 # # ref_compareyr <- "@RN909"
 # # dir_googledrive <- "1vtwfDwRprFml_5wN_WkeVViynwGhC8fe" # https://drive.google.com/drive/folders/1vtwfDwRprFml_5wN_WkeVViynwGhC8fe?usp=sharing
 
-# maxyr <- 2018 # NOTE RAPID RESPONCE
-# strat_yr <- 2019
-# compareyr <- 2016
-# SRVY<-"EBS"
-# ref_compareyr <- "@RN976" # CHANGE
-# dir_googledrive <- "1W8VfqBF9j48vk0GpFLyg5cZGzuHlelAy" # https://drive.google.com/drive/folders/1W8VfqBF9j48vk0GpFLyg5cZGzuHlelAy?usp=sharing
+maxyr <- 2018 # NOTE RAPID RESPONCE
+strat_yr <- 2019
+compareyr <- 2016
+SRVY<-"EBS"
+ref_compareyr <- "@RN976" # CHANGE
+dir_googledrive <- "1W8VfqBF9j48vk0GpFLyg5cZGzuHlelAy" # https://drive.google.com/drive/folders/1W8VfqBF9j48vk0GpFLyg5cZGzuHlelAy?usp=sharing
 
 # maxyr <- 2019
 # compareyr <- 2017
@@ -52,12 +54,12 @@ font0 <- "Arial Narrow"
 # ref_compareyr <- "@Lauth2019" # CHANGE
 # dir_googledrive <- "1HpuuIIp6piS3CieRJR81-8hVJ3QaKOU-" # https://drive.google.com/drive/folders/1HpuuIIp6piS3CieRJR81-8hVJ3QaKOU-?usp=sharing
 
-maxyr <- 2021
-compareyr <- 2019
-strat_yr <- 2019
-SRVY<-"NEBS"
-ref_compareyr <- "@2019NEBS2022" # CHANGE
-dir_googledrive <- "1i3NRmaAPpIYfMI35fpJCa-8AjefJ7J7X" # https://drive.google.com/drive/folders/1i3NRmaAPpIYfMI35fpJCa-8AjefJ7J7X?usp=sharing
+# maxyr <- 2021
+# compareyr <- 2019
+# strat_yr <- 2019
+# SRVY<-"NEBS"
+# ref_compareyr <- "@2019NEBS2022" # CHANGE
+# dir_googledrive <- "1i3NRmaAPpIYfMI35fpJCa-8AjefJ7J7X" # https://drive.google.com/drive/folders/1i3NRmaAPpIYfMI35fpJCa-8AjefJ7J7X?usp=sharing
 
 # maxyr <- 2022
 # compareyr <- 2021
