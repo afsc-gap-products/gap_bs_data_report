c2b9e34c5a3427f727b84565f8bb46d08103c284
emilymarkowitz-noaa <emily.markowitz@noaa.gov>
2022-12-15 10:28:57 -0800

    species narrative maddness

--- code/functions.R
+++ code/functions.R
@@ -1138,7 +1138,7 @@ species_text <- function(
            " was ",
            xunits(sum(biomass_cpue$biomass[biomass_cpue$year == maxyr], na.rm = TRUE), val_under_x_words = NULL), " t (",
            ifelse(show>1, paste0(xunitspct(show), " of the total biomass; "), ""),
-           "Tables ", text_list(paste0("\\@ref(tab:tab_majortaxa_pchange_",SRVY000,")"), "`` `r tab_wt` ``"), ")", 
+           "Tables ", text_list(c(paste0("`` `r tab_majorspp_bio_",SRVY000,"` ``"), "`` `r tab_wt` ``")), ")", 
            " and the estimated abundance was ",
            xunits(sum(biomass_cpue$population[biomass_cpue$year == maxyr], na.rm = TRUE), val_under_x_words = NULL), " fish ",
            # "(Table ` `\\@ref(tab:tab-estimates-maxyr-{{spp_file}}-num)` `). ",
@@ -1152,11 +1152,11 @@ species_text <- function(
                          " of the total biomass; "),
                   ""),
            # "Fig. `` `r fig_dist` `` and ", # 
-           "Tables ", text_list(paste0("\\@ref(tab:tab_majortaxa_pchange_",SRVY000,")"), "`` `r tab_wt` ``"), ")", 
+           "Tables ", text_list(c(paste0("`` `r tab_majorspp_bio_",SRVY000,"` ``"), "`` `r tab_wt` ``")), ")", 
            # text_list(paste0("` `\\@ref(tab:",tab_majorspp_bio,")` `")),
            " and the estimated abundance was ",
            xunits(sum(biomass_cpue$population[biomass_cpue$year == compareyr], na.rm = TRUE), val_under_x_words = NULL), 
-           " fish [",ref_compareyr,"]. ")
+           " fish [`` `r ref_compareyr` ``]. ")
   # 
   #     # Biomass percentage of totSal    
   #     str0$biomass_population_percent <- 
