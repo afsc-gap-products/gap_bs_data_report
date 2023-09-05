969e575bb449a1f42fe91bc8396ee0d2119d3c31
Emily.Markowitz <Emily.Markowitz@nmfs.local>
2023-09-05 00:48:34 -0700

    minor updates 2

--- code/run.R
+++ code/run.R
@@ -70,8 +70,8 @@ ref_compareyr_nbs <- "@RN909" # community report
 # strat_yr <- 2022
 # SRVY<-"NEBS"
 # ref_compareyr <- ref_compareyr_ebs <- "@2022NEBS2023" # CHANGE
-dir_googledrive <- "9ttU1_VAlos_3KKjiRqfcMF-k1cpaENN?usp" # https://drive.google.com/drive/folders/19ttU1_VAlos_3KKjiRqfcMF-k1cpaENN?usp=drive_link
-dir_googledrive_comm <- "1gJYWYWzU8Iwi7gQmoSpCFVfxsoV20P2v" # "https://drive.google.com/drive/folders/1uZy1uDB_poml2KKX3R_Qv8qrWG1WLewE")
+dir_googledrive <- "19ttU1_VAlos_3KKjiRqfcMF-k1cpaENN" 
+dir_googledrive_comm <- "https://drive.google.com/drive/folders/19ttU1_VAlos_3KKjiRqfcMF-k1cpaENN" 
 
 googledrive::drive_deauth()
 googledrive::drive_auth()
