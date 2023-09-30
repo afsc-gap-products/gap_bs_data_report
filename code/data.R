1d0296b3c514cfe003271f8c716f4acc5859279e
Emily.Markowitz <Emily.Markowitz@nmfs.local>
2023-09-30 11:13:46 -0700

    minor edits

--- code/data.R
+++ code/data.R
@@ -981,8 +981,7 @@ biomass_gap <- gap_products_akfin_biomass0 %>%
       n_weight > 0 & 
       year <= maxyr &
       !(species_code %in% c(69323, 69322, 68580, 68560)) &
-      !is.na(species_code)
-  ) %>%  
+      !is.na(species_code) ) %>%  
   unique() %>%
   dplyr::mutate(
     SRVY = dplyr::case_when(
@@ -1015,16 +1014,10 @@ biomass <- dplyr::bind_rows(biomass_gap, biomass_sap) %>%
     cpue_kgkm2_sd = sqrt(cpue_kgkm2_var), 
     cpue_nokm2_sd = sqrt(cpue_nokm2_var), 
     biomass_sd = sqrt(biomass_var), 
-    # biomass_cv = (biomass_sd/biomass_mt), 
-    # biomass_cv_up = biomass_mt + biomass_cv, 
-    # biomass_cv_dw = biomass_mt - biomass_cv, 
     biomass_95ci_up = biomass_mt + (2*biomass_sd), 
     biomass_95ci_dw = biomass_mt - (2*biomass_sd), 
     biomass_95ci_dw = ifelse(biomass_95ci_dw<0, 0, biomass_95ci_dw), 
     population_sd = sqrt(population_var), 
-    # population_cv = (population_sd/population_count), 
-    # population_cv_up = population_count + population_cv, 
-    # population_cv_dw = population_count - population_cv, 
     population_95ci_up = population_count + (2*population_sd), 
     population_95ci_dw = population_count - (2*population_sd), 
     population_95ci_dw = ifelse(population_95ci_dw<0, 0, population_95ci_dw) )
@@ -1037,12 +1030,28 @@ biomass_compareyr <- biomass %>%
   dplyr::filter(stratum == 999) %>%
   dplyr::filter(year == compareyr[1])
 
+temp <- spp_info %>% 
+  dplyr::filter((species_code >= 40000 &
+                    species_code < 99991) |
+                  (species_code > 1 &
+                     species_code < 35000)) %>% 
+  dplyr::select(species_code) %>% 
+  dplyr::distinct() %>%
+  unlist()
+
+temp <- spp_info %>% 
+  dplyr::filter(!((species_code >= 40000 &
+                     species_code < 99991) |
+                    (species_code > 1 & 
+                       species_code < 35000))) %>% 
+  # dplyr::filter(species_code %in% c(99991, 99993, 99994, 99997, 99998, 99999, 1) %>% 
+  dplyr::select(species_code, common_name, species_name)
+
+
 total_biomass <- biomass %>% 
   dplyr::filter(stratum == 999 & 
-                  (species_code >= 40000 &
-                     species_code < 99991) |
-                  (species_code > 1 & 
-                     species_code < 35000)) %>%
+                  !(species_code %in% temp)) %>%
   dplyr::group_by(SRVY, year, taxon) %>% 
   dplyr::summarise(total = sum(biomass_mt, na.rm = T)) %>% 
-  dplyr::ungroup()
+  dplyr::ungroup() %>% 
+  dplyr::arrange(desc(year), desc(taxon))
