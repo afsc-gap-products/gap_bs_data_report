1d483dc5cfd822deb546f6a2292d3ea341d89143
Em Markowitz (NOAA) <emily.markowitz@noaa.gov>
2024-10-23 20:34:38 -0700

    added str_data_changes string creation in data_dl.R to find how many changes have happened in the data since the last data report run

--- code/data_dl.R
+++ code/data_dl.R
@@ -217,3 +217,100 @@ production_sizecomp <- production_sizecomp %>%
 write.csv(x = production_cpue, file = here::here("data/complex_cpue.csv"), row.names = FALSE)
 write.csv(x = production_biomass, file = here::here("data/complex_biomass.csv"), row.names = FALSE)
 write.csv(x = production_sizecomp, file = here::here("data/complex_sizecomp.csv"), row.names = FALSE)
+
+
+# Find changes since data report was last published ----------------------------
+source("https://raw.githubusercontent.com/afsc-gap-products/gap_products/refs/heads/main/functions/summarize_gp_updates.R")
+diff <- summarize_gp_updates(channel = channel_products,
+                             time_start = dl_change_start,
+                             time_end = dl_change_end)  %>%
+  dplyr::filter(SURVEY_DEFINITION_ID %in% c(98, 143))  %>% 
+  dplyr::arrange(SURVEY_DEFINITION_ID) %>%
+  dplyr::mutate(OPERATION_TYPE = dplyr::case_when(
+    OPERATION_TYPE == "UPDATE" & NUMBER_RECS == 1 ~ "update", 
+    OPERATION_TYPE == "INSERT" & NUMBER_RECS == 1 ~ "insertion", 
+    OPERATION_TYPE == "DELETE" & NUMBER_RECS == 1 ~ "deletion", 
+    OPERATION_TYPE == "UPDATE" ~ "updates", 
+    OPERATION_TYPE == "INSERT" ~ "insertions", 
+    OPERATION_TYPE == "DELETE" ~ "deletions"
+  ), 
+  TABLE_NAME_order = dplyr::case_when(
+    TABLE_NAME == "AGECOMP" ~ 4,
+    TABLE_NAME == "SIZECOMP" ~ 3,
+    TABLE_NAME == "BIOMASS" ~ 2,
+    TABLE_NAME == "CPUE" ~ 1
+  ), 
+  TABLE_NAME = dplyr::case_when(
+    TABLE_NAME == "AGECOMP" ~ "age composition",
+    TABLE_NAME == "SIZECOMP" ~ "size composition",
+    TABLE_NAME == "BIOMASS" ~ "biomass",
+    TABLE_NAME == "CPUE" ~ "catch per unit effort"
+  ), 
+  SURVEY_DEFINITION_ID = dplyr::case_when(
+    SURVEY_DEFINITION_ID == 98 ~ "eastern Bering Sea", 
+    SURVEY_DEFINITION_ID == 143 ~ "northern Bering Sea"
+  ))
+
+diff_maxyr <- diff %>%
+  dplyr::filter(YEAR == maxyr) %>%
+  dplyr::group_by(TABLE_NAME, TABLE_NAME_order, OPERATION_TYPE, SURVEY_DEFINITION_ID) %>%
+  dplyr::summarise(NO_RECS = sum(NUMBER_RECS, na.rm = TRUE))  %>% 
+  dplyr::ungroup() %>% 
+  dplyr::arrange(SURVEY_DEFINITION_ID, TABLE_NAME_order)  %>% 
+  dplyr::mutate(year_min = maxyr, 
+                year_max = maxyr)
+
+diff_notmaxyr_years <- diff %>%
+  dplyr::filter(YEAR != maxyr) %>%
+  dplyr::group_by(TABLE_NAME, TABLE_NAME_order, SURVEY_DEFINITION_ID) %>%
+  dplyr::summarise(year_min = min(YEAR, na.rm = TRUE),
+                   year_max = max(YEAR, na.rm = TRUE)) %>% 
+  dplyr::ungroup()
+
+diff_notmaxyr <- diff %>%
+  dplyr::filter(YEAR != maxyr) %>%
+  dplyr::group_by(TABLE_NAME, TABLE_NAME_order, OPERATION_TYPE, SURVEY_DEFINITION_ID) %>%
+  dplyr::summarise(NO_RECS = sum(NUMBER_RECS, na.rm = TRUE)) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::arrange(SURVEY_DEFINITION_ID, TABLE_NAME_order) %>% 
+  dplyr::full_join(diff_notmaxyr_years)
+
+changes_since_string <- function(diff0, str_year, maxyr) {
+  str0 <- c()
+  for (ii in unique(diff0$SURVEY_DEFINITION_ID)) {
+    temp1 <- diff0 %>% 
+      dplyr::filter(SURVEY_DEFINITION_ID == ii)
+    str0 <- paste0(str0, ifelse(ii == unique(diff0$SURVEY_DEFINITION_ID)[1], 
+                                paste0("In ", str_year), "Similarly"), 
+                   ", the ", ii, " ")
+    
+    for (i in unique(diff0$TABLE_NAME)) {
+      temp <- temp1 %>% 
+        dplyr::filter(TABLE_NAME == i) %>% 
+        dplyr::arrange(desc(OPERATION_TYPE))
+      
+      if (temp$year_max[1] == maxyr){
+        str0_years <- c()
+      } else if (temp$year_min[1]==temp$year_max[1]) {
+        str0_years <- paste0(" (", temp$year_min[1], ")")
+      } else {
+        str0_years <- paste0(" (", temp$year_min[1], "-", temp$year_max[1],")") 
+      }
+      
+      str0 <- paste0(str0, 
+                     ifelse(i != unique(diff0$TABLE_NAME)[1], "the ", ""), i, " table observed ", 
+                     text_list(paste0(formatC(x = temp$NO_RECS, digits = 0, big.mark = ","), 
+                                      " ", temp$OPERATION_TYPE)), 
+                     str0_years, 
+                     ifelse(i == unique(diff0$TABLE_NAME)[length(unique(diff0$TABLE_NAME))-1], "; and ", 
+                            ifelse(i == unique(diff0$TABLE_NAME)[length(unique(diff0$TABLE_NAME))], ". ", "; ")))
+    } 
+  }
+  return(str0)
+}
+
+str_maxyr <- changes_since_string(diff0 = diff_maxyr, str_year = maxyr, maxyr = maxyr)
+str_notmaxyr <- changes_since_string(diff0 = diff_notmaxyr, str_year = paste0("the years before ", maxyr), maxyr = maxyr)
+
+str_data_changes <- paste0(str_maxyr, str_notmaxyr)
+writeLines(text = str_data_changes, con = here::here("data", "str_data_changes.txt"))
--- code/run.R
+++ code/run.R
@@ -5,25 +5,6 @@ access_to_internet  <- TRUE # redownload google drive tables and docs?
 quarto <- FALSE
 options(scipen=999)
 
-# maxyr <- 2022
-# compareyr <- 2021
-# compareyr0 <- 2019
-# strat_yr <- 2022
-# SRVY<-"NEBS"
-# ref_compareyr <- ref_compareyr_ebs <- "@2021NEBS2022" # CHANGE
-# # dir_googledrive <- "1x50OKqAyLcqNLYhjQNX84dHLXHcTBnaD" # https://drive.google.com/drive/folders/1x50OKqAyLcqNLYhjQNX84dHLXHcTBnaD
-# ref_compareyr_nbs <- "@RN909" # community report
-# # dir_googledrive_comm <- "1uZy1uDB_poml2KKX3R_Qv8qrWG1WLewE" # "https://drive.google.com/drive/folders/1uZy1uDB_poml2KKX3R_Qv8qrWG1WLewE")
-# 
-# maxyr <- 2023
-# compareyr <- 2022
-# compareyr0 <- 2021
-# strat_yr <- 2022
-# SRVY <- "NEBS"
-# ref_compareyr <- ref_compareyr_ebs <- "@2022NEBS2023" # CHANGE
-# dir_googledrive <- "https://drive.google.com/drive/folders/19ttU1_VAlos_3KKjiRqfcMF-k1cpaENN" 
-# dir_googledrive_comm <- "https://drive.google.com/drive/folders/1gJYWYWzU8Iwi7gQmoSpCFVfxsoV20P2v"
-
 maxyr <- 2024
 compareyr <- 2023
 compareyr0 <- 2023
@@ -31,6 +12,8 @@ strat_yr <- 2022
 SRVY <- "EBS"
 ref_compareyr <- ref_compareyr_ebs <- "@2023NEBS" # CHANGE
 dir_googledrive <- "https://drive.google.com/drive/folders/1uLrsMoJsmGRe8vB0-QSS_6NT8AuWAKsp"
+dl_change_start <- "22-APR-02 11.00.00 PM"
+dl_change_end <- "22-OCT-24 11.59.00 PM"
 
 googledrive::drive_deauth()
 googledrive::drive_auth(); 
